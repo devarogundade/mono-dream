0320a21fec0b1aabf62f1ee7d50e285e4a21515a
Arogundade Ibrahim <devarogundade@gmail.com>
2021-12-13 06:49:29 +0100

    commit

--- rails/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import Mono from "vue-mono";
-
-createApp(App)
-    .use(Mono, { publicKey: "test_pk_7ybniNi3ipmSNMK8zQiU" })
-    .use(router)
-    .mount("#app");
